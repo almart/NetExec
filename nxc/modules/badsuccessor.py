227ed1cbee96e5b02907fb5274f2c9f25d489f1a
Alex Martirosyan <alexmartirosyan@pm.me>
2025-05-24 16:50:39 +0000

    Fix LDAP schema naming context retrieval by using literal for SCOPE_BASE_OBJECT

--- nxc/modules/badsuccessor.py
+++ nxc/modules/badsuccessor.py
@@ -232,8 +232,8 @@ class NXCModule:
             self.context.log.debug(f"Derived domain name: {self.domain_name}")
 
             # Get schema naming context
-            # Corrected ldap.SCOPE_BASE to ldapasn1.SCOPE_BASE_OBJECT
-            root_dse = connection.ldap_connection.search(searchBase="", searchFilter="(objectClass=*)", attributes=["schemaNamingContext"], searchScope=ldapasn1.SCOPE_BASE_OBJECT)
+            # Using literal 0 for SCOPE_BASE_OBJECT due to persistent AttributeError
+            root_dse = connection.ldap_connection.search(searchBase="", searchFilter="(objectClass=*)", attributes=["schemaNamingContext"], searchScope=0)
             parsed_root_dse = parse_result_attributes(root_dse)
             if parsed_root_dse and "schemaNamingContext" in parsed_root_dse[0]:
                 self.schema_naming_context = parsed_root_dse[0]["schemaNamingContext"]
